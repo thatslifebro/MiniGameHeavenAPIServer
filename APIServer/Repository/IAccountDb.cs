cdd55d126ef3065a8f2e8d4ca58505f2c7f8266b
sykim2312 <sykim2312@com2us.com>
2024-01-08 18:19:44 +0900

    fix : String -> string

--- APIServer/Repository/IAccountDb.cs
+++ APIServer/Repository/IAccountDb.cs
@@ -6,7 +6,7 @@ namespace APIServer.Services;
 
 public interface IAccountDb : IDisposable
 {
-    public Task<ErrorCode> CreateAccountAsync(String id, String pw);
+    public Task<ErrorCode> CreateAccountAsync(string id, string pw);
     
-    public Task<Tuple<ErrorCode, Int64>> VerifyUser(String email, String pw);
+    public Task<Tuple<ErrorCode, Int64>> VerifyUser(string email, string pw);
 }
