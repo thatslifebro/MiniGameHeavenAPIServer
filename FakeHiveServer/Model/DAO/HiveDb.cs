b943a4d97f7a9c18ab630b7cde4b5af550188844
sykim2312 <sykim2312@com2us.com>
2024-01-08 18:21:02 +0900

    feat : Add hiveDB DAO & Auth DTO

---
+++ FakeHiveServer/Model/DAO/HiveDb.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace APIServer.Model.DAO;
+
+//AccountDB의 객체는 객체 이름 앞에 Adb를 붙인다.
+
+public class HdbAccountInsert
+{
+    public Int32? player_id { get; set; }
+
+    public string email { get; set; }
+    public string hashed_pw { get; set; }
+    public string salt_value { get; set; }
+    public string? recent_login_dt { get; set; }
+    public string? create_dt { get; set; }
+}
+
+public class HdbAccount
+{
+    public Int32 player_id { get; set; }
+
+    public string email { get; set; }
+    public string hashed_pw { get; set; }
+    public string salt_value { get; set; }
+    public string? recent_login_dt { get; set; }
+    public string create_dt { get; set; }
+}
---
+++ FakeHiveServer/Model/DTO/Auth.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace APIServer.Model.DTO
+{
+    public class CreateHiveAccountRequest
+    {
+        [Required]
+        public string Email { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+    }
+
+    public class CreateHiveAccountResponse
+    {
+        [Required]
+        public ErrorCode Result { get; set; } = ErrorCode.None;
+
+    }
+
+    public class LoginHiveRequest
+    {
+        [Required]
+        public string Email { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+    }
+
+    public class LoginHiveResponse
+    {
+        [Required]
+        public ErrorCode Result { get; set; } = ErrorCode.None;
+        [Required]
+        public Int64 PlayerId { get; set; }
+        [Required]
+        public string Token { get; set; }
+
+    }
+}
