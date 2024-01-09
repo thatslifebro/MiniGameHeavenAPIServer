417dd0305ef1604b020d390f84a020c5b39cc43a
sykim2312 <sykim2312@com2us.com>
2024-01-09 13:42:20 +0900

    fix : IHiveDb valuetuple 변경

--- FakeHiveServer/Repository/IHiveDb.cs
+++ FakeHiveServer/Repository/IHiveDb.cs
@@ -6,6 +6,6 @@ namespace APIServer.Repository
     public interface IHiveDb : IDisposable
     {
         public Task<ErrorCode> CreateAccountAsync(string email, string pw);
-        public Task<Tuple<ErrorCode, Int64>> VerifyUser(string email, string pw);
+        public Task<(ErrorCode, Int64)> VerifyUser(string email, string pw);
     }
 }
