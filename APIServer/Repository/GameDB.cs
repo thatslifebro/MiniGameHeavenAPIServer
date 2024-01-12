27ad5af852649f2cd35b9cfdde466ad164023ebd
sykim2312 <sykim2312@com2us.com>
2024-01-12 11:32:49 +0900

    delete : unused GameDB code

--- APIServer/Repository/GameDB.cs
+++ APIServer/Repository/GameDB.cs
@@ -9,7 +9,6 @@ using Microsoft.Extensions.Options;
 using MySqlConnector;
 using SqlKata.Execution;
 using ZLogger;
-using static APIServer.Controllers.CharacterList;
 
 namespace APIServer.Services;
 
@@ -39,111 +38,6 @@ public class GameDb : IGameDb
     }
 
 
-    public async Task<Tuple<ErrorCode, Int64>> InsertCharacter(Int64 accountId, string nickName)
-    {
-        try
-        {
-            var characterId = await _queryFactory.Query("character").InsertGetIdAsync<int>(new
-            {
-                AccountId = accountId,
-                Nickname = nickName,
-                Exp = 100,
-                Level = 1,
-                Hp = 50,
-                Mp = 60,
-                LocationX = 100,
-                LocationY = 200,
-                Gold = 10000
-            });
-                       
-            return new Tuple<ErrorCode, Int64>(ErrorCode.None, characterId);
-        }
-        catch (Exception ex)
-        {
-            _logger.ZLogError(ex,
-                $"[GameDb.InsertCharacter] ErrorCode : {ErrorCode.CreateCharacterFailException}");
-            return new Tuple<ErrorCode, Int64>(ErrorCode.CreateCharacterFailException, -1);
-        }
-    }
-
-    public async Task<ErrorCode> InsertCharacterItem(Int64 characterId)
-    {
-        try
-        {
-            var count = await _queryFactory.Query("character_item").InsertAsync(new
-            {
-                CharacterId = characterId,
-                Dress = 1,
-                Pants = 1,
-                HairStyle = 1,
-                Mustache = 1,
-                Cloak = 1,
-                Helmet = 1,
-                Armor = 1
-            });
-
-            return ErrorCode.None;
-        }
-        catch (Exception e)
-        {
-            _logger.ZLogError(e,
-                $"[GameDb.InsertCharacterDress ] ErrorCode : {ErrorCode.CreateCharacterRollbackFail}");
-            return ErrorCode.CreateCharacterRollbackFail;
-        }       
-    }
-
-    public async Task<ErrorCode> DeleteCharacter(string nickName)
-    {        
-        try
-        {
-            await _queryFactory.Query("character").Where("NickName", nickName).DeleteAsync();
-            
-            return ErrorCode.None;
-        }
-        catch (Exception e)
-        {
-            _logger.ZLogError(e,
-                $"[GameDb.DeleteCreateCharacter ] ErrorCode : {ErrorCode.CreateCharacterRollbackFail}");
-            return ErrorCode.CreateCharacterRollbackFail;
-        }
-    }
-
-    //캐릭터 아이템 삭제 
-    public async Task<ErrorCode> DeleteCharacterItem(Int64 characterId)
-    {
-        try
-        {
-            await _queryFactory.Query("character_item").Where("CharacterId", characterId).DeleteAsync();
-        
-            return ErrorCode.None;
-        }
-        catch (Exception e)
-        {
-            _logger.ZLogError(e,
-                $"[GameDb.DeleteCreateCharacter ] ErrorCode : {ErrorCode.CreateCharacterRollbackFail}");
-            return ErrorCode.CreateCharacterRollbackFail;
-        }
-    }
-
-    /*public async Task<Tuple<ErrorCode, List<CharacterInfo>>> GetCharacterList(Int64 accountId)
-    {
-        try
-        {
-            var characterList = await _queryFactory.Query("character")
-                .Select("Level", "NickName")
-                .Where("AccountId", accountId)
-                .GetAsync<CharacterInfo>();
-
-            return new Tuple<ErrorCode, IEnumerable<CharacterInfo>>(ErrorCode.None, characterList);
-        }
-        catch (Exception e)
-        {
-            _logger.ZLogError(e,
-                $"[GameDb.GetCharacterList] ErrorCode : {ErrorCode.GetCharacterListFail}");
-            return new Tuple<ErrorCode, IEnumerable<CharacterInfo>>(ErrorCode.GetCharacterListFail, null);
-        }
-        return new Tuple<ErrorCode, List<CharacterInfo>>(ErrorCode.None, null);
-    }   */
     
    
 
