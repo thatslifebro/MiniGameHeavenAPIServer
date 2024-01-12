82359d37680a3b8d8eb267a93e262e492348d5ad
sykim2312 <sykim2312@com2us.com>
2024-01-12 11:47:48 +0900

    fix : FriendList DTO - 최고점수 전달

--- APIServer/Model/DTO/Friend/FriendList.cs
+++ APIServer/Model/DTO/Friend/FriendList.cs
@@ -18,5 +18,6 @@ public class FriendUserInfo
 { 
     public int Uid { get; set; }
     public string NickName { get; set; }
+    public int BestScore { get; set; }
     // 데이터 추가 필요 - 대표 캐릭터 정보, 역대 최고 점수, 최근 접속 일자 등
 }
