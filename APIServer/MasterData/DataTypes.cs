410c89772f5e6836c02f2289f5d53ebf586ade94
sykim2312 <sykim2312@com2us.com>
2024-01-17 16:13:13 +0900

    feat : MasterDB Load to server memory

--- APIServer/MasterData/DataTypes.cs
+++ APIServer/MasterData/DataTypes.cs
@@ -1,7 +1,85 @@
-﻿namespace APIServer.MasterData
+﻿using System.Collections.Generic;
+
+namespace APIServer.MasterData
 {
-    public class ItemData
+    public class AttendanceRewardData
     {
-        public long ItemCode { get; set; }
+        public int day_seq { get; set; }
+        public int reward_key { get; set; }
+        public int reward_qty { get; set; }
+    }
+
+    public class CharacterData
+    {
+        public int char_key { get; set; }
+        public string char_name { get; set; }
+        public string char_grade { get; set; }
+        public int stat_run { get; set; }
+        public int stat_power { get; set; }
+        public int stat_jump { get; set; }
+        public int game_key { get; set; }
+    }
+
+    public class SkinData
+    {
+        public int skin_key { get; set; }
+        public string skin_name { get; set; }
+        public int char_key { get; set; }
+        public int skin_bonus_percent { get; set; }
+    }
+
+    public class CostumeData
+    {
+        public int costume_key { get; set; }
+        public string costume_name { get; set; }
+        public int costume_type { get; set; }
+        public int set_key { get; set; }
+    }
+
+    public class CostumeSetData
+    {
+        public int set_key { get; set; }
+        public int char_key { get; set; }
+        public string set_name { get; set; }
+        public int set_bonus_percent { get; set; }
+        public int char_bonus_percent { get; set; }
+    }
+
+    public class FoodData
+    {
+        public int food_key { get; set; }
+        public string food_name { get; set; }
+        public int game_key { get; set; }
+    }
+
+    public class SkillData
+    {
+        public int skill_key { get; set;}
+        public int act_prob_percent { get; set; }
+        public int char_key { get; set; } = 0;
+    }
+
+    public class GachaRewardData
+    {
+        public GachaRewardInfo gachaRewardInfo { get; set; }
+        public List<GachaReward> gachaRewardList { get; set; }
+    }
+
+    public class GachaRewardInfo
+    {
+        public int gacha_reward_key { get; set; }
+        public int char_prob_percent { get; set; }
+        public int skin_prob_percent { get; set; }
+        public int costume_prob_percent { get; set; }
+        public int food_prob_percent { get; set; }
+        public int food_gear_prob_percent { get; set; }
+        public int gacha_count { get; set; }
+        public string gacha_reward_name { get; set; }
+    }
+
+    public class GachaReward
+    {
+        public int reward_key { get; set; }
+        public string reward_type { get; set; }
     }
 }
--- APIServer/Repository/MasterDb.cs
+++ APIServer/Repository/MasterDb.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data;
+using System.Linq;
 using System.Threading.Tasks;
 using APIServer.MasterData;
 using APIServer.Model.DAO;
@@ -17,7 +18,7 @@ namespace APIServer.Repository;
 
 public class MasterDb : IMasterDb
 {
-    readonly Dictionary<long, ItemData> _itemDatas = new();
+    
     readonly IOptions<DbConfig> _dbConfig;
     readonly ILogger<MasterDb> _logger;
     IDbConnection _dbConn;
@@ -25,6 +26,15 @@ public class MasterDb : IMasterDb
     readonly QueryFactory _queryFactory;
     
     public VersionDAO? _version { get; set; }
+
+    public List<AttendanceRewardData> _attendanceRewardList = new();
+    public List<CharacterData> _characterList = new();
+    public List<SkinData> _skinList = new();
+    public List<CostumeData> _costumeList = new();
+    public List<CostumeSetData> _costumeSetList = new();
+    public List<FoodData> _foodList = new();
+    public List<SkillData> _skillList = new();
+    public List<GachaRewardData> _gachaRewardList = new();
 
     public MasterDb(ILogger<MasterDb> logger, IOptions<DbConfig> dbConfig)
     {
@@ -46,16 +56,36 @@ public class MasterDb : IMasterDb
     {
         try
         {
-            //_ = LoadItem(queryFactory);
             _version = await _queryFactory.Query($"version").FirstOrDefaultAsync<VersionDAO>();
+            _attendanceRewardList = (await _queryFactory.Query($"master_attendance_reward").GetAsync<AttendanceRewardData>()).ToList();
+            _characterList = (await _queryFactory.Query($"master_char").GetAsync<CharacterData>()).ToList();
+            _skinList = (await _queryFactory.Query($"master_skin").GetAsync<SkinData>()).ToList();
+            _costumeList = (await _queryFactory.Query($"master_costume").GetAsync<CostumeData>()).ToList();
+            _costumeSetList = (await _queryFactory.Query($"master_costume_set").GetAsync<CostumeSetData>()).ToList();
+            _foodList = (await _queryFactory.Query($"master_food").GetAsync<FoodData>()).ToList();
+            _skillList = (await _queryFactory.Query($"master_skill").GetAsync<SkillData>()).ToList();
+
+            var gachaRewards = await _queryFactory.Query($"master_gacha_reward").GetAsync<GachaRewardInfo>();
+            foreach (var gachaRewardInfo in gachaRewards)
+            {
+                GachaRewardData gachaRewardData = new();
+                gachaRewardData.gachaRewardInfo = gachaRewardInfo;
+                gachaRewardData.gachaRewardList = (await _queryFactory.Query("master_gacha_reward_list")
+                                                   .Where("gacha_reward_key", gachaRewardInfo.gacha_reward_key)
+                                                   .GetAsync<GachaReward>())
+                                                   .ToList();
+                _gachaRewardList.Add(gachaRewardData);
+            }
+
         }
         catch(Exception e)
         {
+            Console.WriteLine(e);
             _logger.ZLogError(e,
                 $"[MasterDb.Load] ErrorCode: {ErrorCode.MasterDB_Fail_LoadData}");
             return false;
         }
-        if(!ValidateMasterData())
+        if (!ValidateMasterData())
         {
             _logger.ZLogError($"[MasterDb.Load] ErrorCode: {ErrorCode.MasterDB_Fail_InvalidData}");
             return false;
@@ -67,7 +97,15 @@ public class MasterDb : IMasterDb
 
     bool ValidateMasterData()
     {
-        if (_version == null)
+        if (_version == null || 
+            _attendanceRewardList.Count == 0 ||
+            _characterList.Count == 0 ||
+            _skinList.Count == 0 ||
+            _costumeList.Count == 0 || 
+            _costumeSetList.Count == 0 ||
+            _foodList.Count == 0 ||
+            _skillList.Count == 0 ||
+            _gachaRewardList.Count == 0)
         {
             return false;
         }
