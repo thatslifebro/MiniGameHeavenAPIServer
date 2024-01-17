d8a9c1bc8171d953521908f3789b15e23cb16b80
sykim2312 <sykim2312@com2us.com>
2024-01-17 10:44:53 +0900

    fix : DAO - rename files and add other gameDB DAO

---
+++ APIServer/Model/DAO/GameDB/Attendance.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace APIServer.Model.DAO.GameDB
+{
+    public class GdbAttendance
+    {
+        public int uid { get; set; }
+        public int attendance_count { get; set; }
+        public DateTime recent_attendance_dt { get; set; }
+    }
+}
---
+++ APIServer/Model/DAO/GameDB/Mailbox.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace APIServer.Model.DAO.GameDB
+{
+    public class GdbMailboxInfo
+    {
+        public int mail_seq { get; set; }
+        public int uid { get; set; }
+        public string mail_title { get; set; }
+        public DateTime create_dt { get; set; }
+        public DateTime expire_dt { get; set; }
+        public DateTime receive_dt { get; set; }
+    }
+
+    public class GdbMailboxRewardInfo
+    {
+        public int mail_seq { get; set; }
+        public int reward_key { get; set; }
+        public int reward_qty { get; set; }
+        public int reward_type { get; set; }
+    }
+}
